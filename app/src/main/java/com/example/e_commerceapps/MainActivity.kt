0fd84f0e4a3e1e84bf566b1e07c26400ddf96924
umut <diler.2029@gmail.com>
2024-11-21 22:53:34 +0300

    bottomNav and menu added

--- app/src/main/java/com/example/e_commerceapps/MainActivity.kt
+++ app/src/main/java/com/example/e_commerceapps/MainActivity.kt
@@ -5,12 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.e_commerceapps.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+
+        bottomNav = binding.bottomNavigationView
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        bottomNav.setupWithNavController(navHostFragment.navController)
+
+        hideBottomNav()
+
+    }
+
+    fun showBottomNav() {
+        binding.bottomNavigationView.visibility = BottomNavigationView.VISIBLE
+    }
+    fun hideBottomNav() {
+        binding.bottomNavigationView.visibility = BottomNavigationView.GONE
+    }
+
+    private fun initView(){
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
